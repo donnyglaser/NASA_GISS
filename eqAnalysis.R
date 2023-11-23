e6739e4e7084820fa4ab85d3cec039b4dbff59d5
DonnyGlaser <donnymglaser@gmail.com>
2023-11-23 15:01:58 -0500

    completed script, need to test

--- eqAnalysis.R
+++ eqAnalysis.R
@@ -16,6 +16,7 @@ diagList <- c(diagList, 'aij')
 diagList <- c(diagList, 'aijk')
 diagList <- c(diagList, 'aijl')
 ## OCEAN ##
+diagList <- c(diagList, 'oij')
 diagList <- c(diagList, 'oijl')
 
 ## Extract name of run ##
@@ -35,20 +36,28 @@ monList <- c('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT
 ## Generic subset of specific latitudes ##
 latList <- c(90, 78, 62, 46, 30, 14, 2, -14, -30, -46, -62, -78, -90)
 
-## This array needs to be customized for specific diagnostics, these are a select set for aij files ##
+## select variables to analyze equilibrium in aij, aijk, aijl, oil, and oijl diagnostics ## edit if you like
 allVar <- data.frame()
 allVar <- rbind(allVar, cbind('aij', c("bs_snowdp", "gice", "gwtr", "incsw_toa", "landicefr", "prsurf", "qatm", "snowdp", "srtrnf_grnd", "tgrnd", "tsurf", "wsurf", "zsnow")))
 allVar <- rbind(allVar, cbind('aijl', c('q', 'rh', 'temp')))
 allVar <- rbind(allVar, cbind('aijk',c('tb')))
+allVar <- rbind(allVar, cbind('oij', c('oij_hbl', 'oij_mld')))
 allVar <- rbind(allVar, cbind('oijl', c('heat', 'pot_temp', 'salt')))
 colnames(allVar) <- c('Diagnostic', 'Variable')
+
+## select atmos and ocean layers ##  edit if you like
+aLayers <- c(1, 2, 4, 9, 14, 26, 35, 39)
+oLayers <- c(1, 2, 3, 8, 13)
+
 
 outFrame <- data.frame()
 for(iDiag in 1:length(diagList)) {
     diagFiles <- list.files(getwd(), pattern = diagList[iDiag])
 
-    for(iYear in YearI:YearE) {
-        if(iYear > 99){
+    for(iYear in YearI:YearE) { # handles up to 9999 years
+        if(iYear > 999) {
+            yearL <- diagFiles[grepl(paste0(iYear, '.'), diagFiles)]
+        } else if(iYear > 99){
             yearL <- diagFiles[grepl(paste0('0', iYear, '.'), diagFiles)]
         } else if(iYear > 9) {
             yearL <- diagFiles[grepl(paste0('00', iYear, '.'), diagFiles)]
@@ -56,7 +65,7 @@ for(iDiag in 1:length(diagList)) {
             yearL <- diagFiles[grepl(paste0('000', iYear, '.'), diagFiles)]
         }
 
-        if((length(yearL)) < 12) {
+        if((length(yearL)) < 12) { # handles partial years (if < 12 months)
             if(length(yearL) == 1) {
                 break
             } else {
@@ -75,38 +84,85 @@ for(iDiag in 1:length(diagList)) {
 
             varList <- subset(allVar, Diagnostic %in% diagList[iDiag])
             for(iVar in 1:length(varList)) {
-                t_data <- ncvar_get(nc_data, varid=varList[iVar])
+                t_data <- ncvar_get(nc_data, varid=varList[iVar,2])
 
                 ## need to add a bit here to add z levels ##
                 ## if else ##
-                
-                colnames(t_data) <- seq(-90,90,4)
-                row.names(t_data) <- seq(-177.5,177.5,5)
-                t_data <- melt(t_data)
-                colnames(t_data) <- c('lon', 'lat', 'value')
+                if(diagList[iDiag] %in% 'aij') {
+                    ## surface layer only ##
+                    colnames(t_data) <- seq(-90,90,4)
+                    row.names(t_data) <- seq(-177.5,177.5,5)
+                    t_data <- melt(t_data)
+                    colnames(t_data) <- c('lon', 'lat', 'value')
 
-                for(iLat in 1:length(latList)) {
-                    t <- subset(t_data, lat == latList[iLat])
-                    tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], latList[iLat], varList[iVar], mean(t$value), sd(t$value))
+                    for(iLat in 1:length(latList)) {
+                        t <- subset(t_data, lat == latList[iLat])
+                        tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], latList[iLat], 'AtmosSurface', varList[iVar], mean(t$value, na.rm=TRUE), sd(t$value, na.rm=TRUE))
+                        outFrame <- rbind(outFrame, tOut)
+                    }
+
+                    ## Global and Hemisphere Averages ##
+                    hemiData <- ncvar_get(nc_data, varid=paste0(varList[iVar], '_hemis'))
+                    tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], 'SouthernHemisphere', 'AtmosSurface', varList[iVar], hemiData[1], 0)
                     outFrame <- rbind(outFrame, tOut)
-                }
+                    tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], 'NorthernHemisphere', 'AtmosSurface', varList[iVar], hemiData[2], 0)
+                    outFrame <- rbind(outFrame, tOut)
+                    tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], 'Global', 'AtmosSurface', varList[iVar], hemiData[3], 0)
+                    outFrame <- rbind(outFrame, tOut)
 
-                ## Global and Hemisphere Averages ##
-                hemiData <- ncvar_get(nc_data, varid=paste0(varList[iVar], '_hemis'))
-                tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], 'SouthernHemisphere', varList[iVar], hemiData[1], 0)
-                outFrame <- rbind(outFrame, tOut)
-                tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], 'NorthernHemisphere', varList[iVar], hemiData[2], 0)
-                outFrame <- rbind(outFrame, tOut)
-                tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], 'Global', varList[iVar], hemiData[3], 0)
-                outFrame <- rbind(outFrame, tOut)
+                } else if(diagList[iDiag] %in% 'oij') {
+                    ## surface layer only ##
+                    colnames(t_data) <- seq(-90,90,4)
+                    row.names(t_data) <- seq(-177.5,177.5,5)
+                    t_data <- melt(t_data)
+                    colnames(t_data) <- c('lon', 'lat', 'value')
+
+                    for(iLat in 1:length(latList)) {
+                        t <- subset(t_data, lat == latList[iLat])
+                        tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], latList[iLat], 'OceanSurface', varList[iVar], mean(t$value, na.rm=TRUE), sd(t$value, na.rm=TRUE))
+                        outFrame <- rbind(outFrame, tOut)
+                    }
+
+                } else if(diagList[iDiag] %in% 'oijl') {
+                    ## 13 ocean layers ##
+                    for(iLayer in 1:length(oLayers)) {
+                        t_data <- t_data[,,oLayers[iLayer]]
+                        colnames(t_data) <- seq(-90,90,4)
+                        row.names(t_data) <- seq(-177.5,177.5,5)
+                        t_data <- melt(t_data)
+                        colnames(t_data) <- c('lon', 'lat', 'value')
+
+                        for(iLat in 1:length(latList)) {
+                            t <- subset(t_data, lat == latList[iLat])
+                            tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], latList[iLat], paste0('OceanLayer_', oLayers[iLayer]), varList[iVar], mean(t$value, na.rm=TRUE), sd(t$value, na.rm=TRUE))
+                            outFrame <- rbind(outFrame, tOut)
+                        }
+                    }
+
+                } else { ## aijk and aijk
+                    ## 40 atmos layers ##
+                    for(iLayer in 1:length(aLayers)) {
+                        t_data <- t_data[,,aLayers[iLayer]]
+                        colnames(t_data) <- seq(-90,90,4)
+                        row.names(t_data) <- seq(-177.5,177.5,5)
+                        t_data <- melt(t_data)
+                        colnames(t_data) <- c('lon', 'lat', 'value')
+
+                        for(iLat in 1:length(latList)) {
+                            t <- subset(t_data, lat == latList[iLat])
+                            tOut <- cbind(runName, iYear, sprintf('%02s', iMon), monList[iMon], latList[iLat], paste0('OceanLayer_', aLayers[iLayer]), varList[iVar], mean(t$value, na.rm=TRUE), sd(t$value, na.rm=TRUE))
+                            outFrame <- rbind(outFrame, tOut)
+                        }
+                    }
+                }
             }
             nc_close(nc_data)
         }
     }
 }
 
-colnames(outFrame) <- c('Run_ID', 'Year', 'Month_Num', 'Month_Name', 'Latitude', 'Variable', 'Mean_Value', 'SD_Value') ## add level column (surface/level#/pressure)
-outFrame[,c(2,5,7:8)] <- apply(outFrame[,c(2,5,7:8)], 2, as.numeric)
+colnames(outFrame) <- c('Run_ID', 'Year', 'Month_Num', 'Month_Name', 'Latitude', 'Layer', 'Variable', 'Mean_Value', 'SD_Value') 
+outFrame[,c(2,5,7:8)] <- apply(outFrame[,c(2,5,8:9)], 2, as.numeric)
 outFrame <- outFrame %>% mutate(Time = as.POSIXct(paste0('00', Year, '-', Month_Num, '-', 01, ' ', 00, ':', 00, ':', 00), format='%Y-%m-%d %H:%M:%OS'))
 
 saveRDS(outFrame, file = paste0(runName, '_Equilibrium_', format(Sys.time(), "%y%m%d"), '.rds'))
