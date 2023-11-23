fc89133d1e2cbbe9b86108555ac4055a8b99abd7
DonnyGlaser <donnymglaser@gmail.com>
2023-11-22 20:01:16 -0500

    add RUNID slurm start script

---
+++ RUNID_st.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --account=s1001
+
+#SBATCH --nodes=1
+
+#SBATCH --ntasks-per-node=44
+
+#SBATCH --time=12:00:00
+
+#SBATCH --output=./slurmOut/%x-%j-slurm.out
+
+##SBATCH --dependency=afterany:
+
+##################
+RUNID=RUNID
+##################
+
+module purge
+
+#module load nco/4.8.1
+
+module load comp/intel/2021.3.0
+
+module load mpi/impi/2021.3.0
+
+#module load xxdiff/4.0.1
+
+ 
+
+rm /discover/nobackup/projects/giss_ana/users/dmglaser/model_out/${RUNID}/lock
+
+cd /home/dmglaser/modelE/exec
+
+./runE ${RUNID} -np 44
