62cda5a1447924483482a1d72785aaf242ccd27e
DonnyGlaser <donnymglaser@gmail.com>
2023-11-24 13:29:57 -0500

    change to only scale aij

---
+++ massScaleACC_MarsPLD.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+diags=()
+## Remove comment for the desired diagnostics ##
+
+# ATMOS #
+diags+=("aij")
+#diags+=("aijk")
+#diags+=('aijl')
+#diags+=('taij')
+#diags+=('taijl')
+
+# OCEAN #
+#diags+=('oij')
+#diags+=('oijk')
+#diags+=('oijl')
+#diags+=('toij')
+#diags+=('toijl')
+
+## SUBDD ##
+#diags+=('aijph2')
+#diags+=('taijph2')
+#diags+=('oijph2')
+#diags+=('toijph2')
+
+# list all .acc files
+accAll=`ls ./*.acc*`
+x=${accAll[0]:13:1}
+
+for ifile in $accAll ## this isnt working like I thought
+do
+    for idiag in "${diags[@]}"
+    do
+        scaleacc $ifile $idiag
+    done
+done
+
+## makes a directory and moves the files
+mkdir diagOut
+
+for i in "${diags[@]}"
+do
+        target="*."$i$x"*"
+        mv -f $target ./diagOut/
+done
+
+## Remove Partial Files ##
+cd diagOut
+rm PARTIAL.*
