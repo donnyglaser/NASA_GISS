662be99ebcbb34d8995f1f3a607144b0c0b073a8
DonnyGlaser <donnymglaser@gmail.com>
2023-12-07 16:05:40 -0500

    working generic netCDF editor

--- EditnetCDF.R
+++ EditnetCDF.R
@@ -2,6 +2,8 @@
 
 library(ncdf4) # package for netcdf manipulation
 
+setwd('') # directory containing netCDF file
+
 origName <- '' # insert the filename of the input file to be edited here
 newName <- '' # insert new filename here
 
@@ -17,13 +19,16 @@ varNames <- names(template[['var']]) # names of variables
 
 editVar <- '' # insert the name of the variable to be edited here
 
-varid <- template[['var']][[editVar]] # 
+varID <- template[['var']][[editVar]] # variable metadata e.g., dimensions, units, etc.
 
-vartab <- ncvar_get(template, varid= editVar)
+varTab <- ncvar_get(template, varid= editVar) # variable matrix (check dimensions; can be a 2, 3, or 4 dimension matrix)
 
 ### EDIT THE VARIABLE ###
 
-ncvar_put(template, varid, vartab)
+ncvar_put(template, varID, varTab)
+
+## EXAMPLE ##
+# varTab[1:72, 1:46] <- 0
 
 ### REPEAT AS NEEDED FOR EACH VARIABLE ###
 
@@ -35,14 +40,16 @@ ncvar_put(template, varid, vartab)
 
 attNames <- names(ncatt_get(template,0)) # names of metadata
 
-editAtt <- '' # insert the name of the metadata to be edited here
+editAtt <- '' # insert the name (title) of the metadata to be edited here
 
 meta <- '' # metadata here
+
+### EDIT THE METADATA ###
 
 ncatt_put(template, 0, editAtt, meta)
 
 ## EXAMPLE ##
-ncatt_put(template, 0, 'Date created', format(Sys.time(), "%b %d %Y")) # timestamp
+# ncatt_put(template, 0, 'Date created', format(Sys.time(), "%b %d %Y")) # timestamp
 
 ### REPEAT AS NEEDED FOR ALL METADATA ###
 
