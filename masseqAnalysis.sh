3e3b14ab2fa913aa71a23c2c3c6c2495ac284a36
DonnyGlaser <donnymglaser@gmail.com>
2024-03-06 12:19:57 -0500

    copy and run eqAnalysis in each topo ens run folder

---
+++ masseqAnalysis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# run in nobackup model_out
+
+list=`ls -d TopoEns_*_a/`
+
+for ilist in "${list[@]}"
+do
+        cd "$ilist"
+        cp /discover/home/dmglaser/modelE/decks/scripts/eqAnalysis.* .
+        sbatch eqAnalysis.sh
+        cd..
+done
