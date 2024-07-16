e6153d67caf5a192ee5b8f31bee1eb59903c71d7
DonnyGlaser <donnymglaser@gmail.com>
2024-07-16 10:49:41 -0400

    removed inefficient rbind data write

--- eqAnalysisGlob_single_discover.R
+++ eqAnalysisGlob_single_discover.R
@@ -1,3 +1,6 @@
+## takes annual files, and extracts target diagnostics for analysis ##
+## run this after runSumScale.sh ##
+## run this in the ANN folder ##
 
 
 ## Libraries ##
@@ -16,36 +19,41 @@ runName <- gsub('aij', '', runName)
 
 diags <- c('tsurf_hemis', 'net_rad_planet_hemis', 'qatm_hemis', 'pcldt_hemis', 'prsurf_hemis', 'gwtr_hemis', 'gice_hemis', 'prec_hemis', 'pot_evap_hemis', 'ZSI_hemis', 'snowdp_hemis')
 
-dataOut <- data.frame()
-for(ifile in 1:length(aij)) {
+nifile <- length(aij)
+nidiag <- length(diags)
+
+dataOut <- data.frame(matrix(NA, nrow = ((nifile*nidiag)), ncol = 4))
+for(ifile in 1:nifile) {
     nc <- nc_open(aij[ifile])
 
-    for(idiag in 1:length(diags)) {
+    for(idiag in 1:nidiag) {
         tDiag <- ncvar_get(nc, varid = diags[idiag])
         tDiag <- tDiag[3]
 
-        tOut <- cbind(runName, ifile, diags[idiag], tDiag)
-        colnames(tOut) <- c('RunName', 'Year', 'Diagnostic', 'Value')
-        dataOut <- rbind(dataOut, tOut)
+        dataOut[(((ifile - 1)*nidiag) + idiag),] <- cbind(runName, ifile, diags[idiag], tDiag)
     }
     nc_close(nc)
-    print(ifile) ## to see progress
+    print(paste0(ifile, '/', nifile)) ## to see progress
 }
+colnames(dataOut) <- c('RunName', 'Year', 'Diagnostic', 'Value')
 
 eqNC <- list.files(getwd(), pattern = 'aij')
 eqNC <- eqNC[grepl('-', eqNC)]
 eqNC <- eqNC[length(eqNC)]
 
 nc <- nc_open(eqNC)
-for(idiag in 1:length(diags)) {
+dataOut2 <- data.frame(matrix(NA, nrow = (nidiag), ncol = 4))
+for(idiag in 1:nidiag) {
     tDiag <- ncvar_get(nc, varid = diags[idiag])
     tDiag <- tDiag[3]
 
     tOut <- cbind(runName, 'eq50', diags[idiag], tDiag)
     colnames(tOut) <- c('RunName', 'Year', 'Diagnostic', 'Value')
-    dataOut <- rbind(dataOut, tOut)
+    dataOut2 <- rbind(dataOut, tOut)
 }
 nc_close(nc)
+
+dataOut <- rbind(dataOut, dataOut2)
 
 saveName <- paste0(runName, '_EqGlobal_', format(Sys.time(), "%y%m%d"))
 saveRDS(dataOut, file = paste0(saveName, '.rds'))
